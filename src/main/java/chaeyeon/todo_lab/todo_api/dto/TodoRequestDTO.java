c365f51e3200615e6a0f89fa5a061c01d67167e2
i-chaeyeon <leechae0605@naver.com>
2025-04-25 14:04:25 +0900

    feat: TodoRequestDTO 클래스 내 유효성 검증 추가
    - validation constraints

--- src/main/java/chaeyeon/todo_lab/todo_api/dto/TodoRequestDTO.java
+++ src/main/java/chaeyeon/todo_lab/todo_api/dto/TodoRequestDTO.java
@@ -3,15 +3,24 @@ package chaeyeon.todo_lab.todo_api.dto;
 import chaeyeon.todo_lab.todo_api.domain.Importance;
 import chaeyeon.todo_lab.todo_api.domain.Todo;
 import chaeyeon.todo_lab.todo_api.util.IdGenerator;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class TodoRequestDTO {
+    @NotBlank(message = "할일을 입력해주세요.")
     private final String title;
+
+    @Future(message = "마감일은 현재 이후로 지정해주세요.")
     private final LocalDate dueDate;
+
+    @NotBlank(message = "카테고리를 입력해주세요.")
     private final String category;
+
     private final Importance importance;
+
     private final Boolean isDone;
 
     public TodoRequestDTO(String title, LocalDate dueDate, String category, Importance importance, Boolean isDone) {
